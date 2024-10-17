41d4ef1f0554bb49beac2ed26fd16fe1bf96146b
Alfia Parvez <45048215+alfia14@users.noreply.github.com>
2024-10-17 12:51:16 -0500

    Add files via upload

--- src/database/db.js
+++ src/database/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 // Create a MySQL connection
 const connection = mysql.createConnection({
-  host: 'localhost', // If hosted on a remote server, use the server's IP
+  host: '18.220.149.166', // If hosted on a remote server, use the server's IP
   user: 'nhblstudies_alfiap', // Your MySQL user
   password: 'Alfia@123', // Your MySQL password
   database: 'nhblstudies_user_authentication', // The database name
